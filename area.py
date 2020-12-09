99397efe3c2d312489f29eb54d37229675da3a70
aiswariyak <74720701+aiswariyak@users.noreply.github.com>
2020-12-09 07:38:08 -0800

    Add files via upload

---
+++ area.py
@@ -0,0 +1,9 @@
+from math import pi
+r=float(input("enter the radius of the circle"))
+A=pi*r*r
+print("Area of the circle is:",A)
+
+
+
+
+
---
+++ simpleinterest.py
@@ -0,0 +1,5 @@
+p=float(input("enter principle amount"))
+r=float(input("enter annual interest rate"))
+t=int(input("enter  time in years"))
+s=(p*r*t)/100
+print("simple interest is :",s)
