fef49bce5d58a39903967245e33987a4ddf52b52
aiswariyak <74720701+aiswariyak@users.noreply.github.com>
2021-02-26 12:54:50 +0530

    Add files via upload

---
+++ package_programs.api/graphics/threedgraphics/cuboid.py
@@ -0,0 +1,4 @@
+def cuboid_area(l,b,h):
+    return 2*(l*h + b*h + l*b)
+def cuboid_perimeter(l,b,h):
+    return 4*(l+b+h)
---
+++ package_programs.api/graphics/threedgraphics/sphere.py
@@ -0,0 +1,5 @@
+from math import pi
+def sphere_area(r):
+    return 4*pi*r*r
+def sphere_perimeter(r):
+    return 2*pi*r
